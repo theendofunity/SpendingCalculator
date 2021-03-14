95c8945a6dea45678d65704b8f4847a1cbccb5c9
theendofunity <dwd96@yandex.ru>
2021-03-14 18:17:39 +0300

    Added UI

--- SpendCalculator/ViewController.swift
+++ SpendCalculator/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var itemTextEdit: UITextField!
+    @IBOutlet weak var priseTextEdit: UITextField!
+    @IBOutlet weak var spendTableView: UITableView!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func add(_ sender: UIButton) {
+    }
+    
 }
 
