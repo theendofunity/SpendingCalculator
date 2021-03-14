362bdb1f1408c0313efa8ec8f3ae784df3079ee3
theendofunity <dwd96@yandex.ru>
2021-03-14 19:10:35 +0300

    Adding/clearing table view

--- SpendCalculator/ViewController.swift
+++ SpendCalculator/ViewController.swift
@@ -7,20 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var itemTextEdit: UITextField!
     @IBOutlet weak var priseTextEdit: UITextField!
     @IBOutlet weak var spendTableView: UITableView!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    var spends = [(item: String, prise: Int)]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        spendTableView.dataSource = self
     }
 
     @IBAction func add(_ sender: UIButton) {
+        let itemName = itemTextEdit.text ?? "-"
+        guard let prise = Int(priseTextEdit.text ?? "0") else {
+            return
+        }
+        
+        spends.append((itemName, prise))
+        spendTableView.reloadData()
     }
     
+    @IBAction func clear(_ sender: UIButton) {
+        spends.removeAll()
+        spendTableView.reloadData()
+    }
+    
+    //MARK: - Table view data sourse
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spends.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "spendCell", for: indexPath)
+        
+        let spend = spends[indexPath.row]
+        cell.textLabel?.text = "\(spend.item)       \(spend.prise)"
+       
+        return cell
+    }
 }
 
